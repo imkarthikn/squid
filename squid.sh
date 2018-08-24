3b4bc94f4dfc686711c5d79ec36b5281ca164d28
Cloud User <centos@ip-10-0-1-11.ec2.internal>
2018-08-24 11:25:44 +0100

    scipt to attach interface

---
+++ squid.sh
@@ -0,0 +1,16 @@
+count=$(cat /proc/net/dev | grep -i ens | awk -F ":" '{print $1}' | grep -v ens5  | tr -d " \t" | wc -l)
+if [ "$count" -eq "0" ];
+then 
+interface=eth0
+else
+interface=$(cat /proc/net/dev | grep -i ens | awk -F ":" '{print $1}' | grep -v ens5  | tr -d " \t")
+fi
+echo $interface
+sed -i '/HWADDR=/d' /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "HWADDR=" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i '/DEVICE=/d' /etc/sysconfig/network-scripts/ifcfg-eth0
+echo -e "DEVICE=$interface" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+sudo /sbin/ifup $interface
+sleep 30
+sudo /sbin/ifdown ens5
+
